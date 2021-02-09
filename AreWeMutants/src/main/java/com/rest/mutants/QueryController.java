e42b291520e140b57c7e2b68096758bac682fce8
purplecore11 <purplecore@gmail.com>
2021-02-09 11:38:59 -0500

    Changes for level 2 and 3

--- AreWeMutants/src/main/java/com/rest/mutants/QueryController.java
+++ AreWeMutants/src/main/java/com/rest/mutants/QueryController.java
@@ -2,6 +2,7 @@ package com.rest.mutants;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +13,48 @@ import com.mutants.DNAValidator;
 public class QueryController {
 
   private final CreatureRepository repository;
-  private final CreatureModelAssembler assembler;
 
-  QueryController(CreatureRepository repository, CreatureModelAssembler assembler) {
+  QueryController(CreatureRepository repository) {
     this.repository = repository;
-    this.assembler = assembler;
   }
   
+  
   @PostMapping("/mutant")
-  boolean isMutant(@RequestBody DNASequence sequence, HttpServletResponse response) {
-	  boolean result = DNAValidator.isMutant(sequence.getDna());
+  String isMutant(@RequestBody DNASequence sequence, HttpServletResponse response) {
+	  Creature creature = repository.findByDNA(sequence.toString());
+	  boolean result = false;
+	  
+	  if(creature != null)
+	  {
+		  result = creature.getCreatureClass().equals("Mutant") ? true : false;
+	  }
+	  else
+	  {
+		  result = DNAValidator.isMutant(sequence.getDna());
+		  creature = new Creature(sequence.toString(), result?"Mutant":"Human");
+		  repository.save(creature);
+	  }
 	  
 	  if(!result)
 		  response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 	  else
 		  response.setStatus(HttpServletResponse.SC_OK);
+
+	  return result ? "Mutant!!" : "Not mutant";
+  }
+  
+  @GetMapping("/stats")
+  Statistics getStatistics() {
+	  Integer humans = repository.getNumberofHumans();
+	  Integer mutants = repository.getNumberofMutants();
+	  Float ratio = humans > 0 ?
+			  (float)mutants/(float)humans
+			  : 0;
 	  
-	  return result;
+	  Statistics stats = new Statistics(mutants,
+			  humans,
+			  ratio);
+	  
+	  return stats;
   }
 }
