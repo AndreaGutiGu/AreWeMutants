8407143c448cfaa9d1e338f05fffc0493bcf71ad
purplecore11 <purplecore@gmail.com>
2021-02-10 09:31:59 -0500

    Changes to use MySQL at different location+

--- AreWeMutants/src/main/java/com/rest/mutants/CreatureRepository.java
+++ AreWeMutants/src/main/java/com/rest/mutants/CreatureRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 interface CreatureRepository extends JpaRepository<Creature, Long> {
 	Creature findByDNA(String DNA);
 
-	@Query(value = "SELECT COUNT(c.id) FROM mutantes.creature c WHERE c.creature_class = 'Mutant'", nativeQuery = true)
+	@Query(value = "SELECT COUNT(c.id) FROM creature c WHERE c.creature_class = 'Mutant'", nativeQuery = true)
 	Integer getNumberofMutants();
 
-	@Query(value = "SELECT COUNT(c.id) FROM mutantes.creature c WHERE c.creature_class = 'Human'", nativeQuery = true)
+	@Query(value = "SELECT COUNT(c.id) FROM creature c WHERE c.creature_class = 'Human'", nativeQuery = true)
 	Integer getNumberofHumans();
 
 }
