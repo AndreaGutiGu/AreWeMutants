90ac53a85e7e69d6612e5c6d38017dfb73106891
purplecore11 <purplecore@gmail.com>
2021-06-22 11:49:20 -0500

    Modifying test to be called automatically

--- AreWeMutants/src/test/java/com/rest/mutants/creature/CreatureRepositoryIntegrationTest.java
+++ AreWeMutants/src/test/java/com/rest/mutants/creature/CreatureRepositoryIntegrationTest.java
@@ -2,7 +2,7 @@ package com.rest.mutants.creature;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
